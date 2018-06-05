a0209a834cfa401000e44d44777b9f2a62ab1a40
NOSS <28915482+noss3885@users.noreply.github.com>
2018-06-05 23:23:09 +0900

    add modular inverse

--- src/math/extendedEuclid.cpp
+++ src/math/extendedEuclid.cpp
@@ -1,12 +1,12 @@
 /*
 拡張ユークリッド互除法
-計算量
+計算量 O(log(m)^2)
 ax+by=gcd(a,b) となるx,yを求める
+modの逆元を求める
 */
-int extgcd(int a, int b, int &x, int &y)
-{
+
+int extgcd(int a, int b, int &x, int &y) {
     int d = a;
- 
     if (b == 0) {
         x = 1;
         y = 0;
@@ -14,6 +14,11 @@ int extgcd(int a, int b, int &x, int &y)
         d = extgcd(b, a % b, y, x);
         y -= (a / b) * x;
     }
- 
     return d;
+}
+
+int mod_inv(int a, int m) {
+    int a_, q;
+    extgcd(a, m, a_, q);
+    return a_;
 }
