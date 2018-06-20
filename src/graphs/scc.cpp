3e17a8cf54a2a96d91a4b0c692ac4be7631701fc
NOSS <28915482+noss3885@users.noreply.github.com>
2018-06-20 18:43:09 +0900

    add Strongly Connected Components

---
+++ src/graphs/scc.cpp
@@ -0,0 +1,56 @@
+/*
+強連結成分分解(Strongly-Connected-Components)
+計算量 O(V+E)
+*/
+#include <vector>
+#include <stack>
+using namespace std;
+
+typedef pair<int, int> P;
+
+struct SCC {
+    vector<vector<int> > g, gr;
+    vector<int> visited, num;
+    vector<P> es;
+    stack<int> st;
+
+    SCC(int n) : g(n), gr(n), visited(n, 0), num(n, -1) {}
+
+    void add_edge(int u, int v) {
+        g[u].push_back(v);
+        gr[v].push_back(u);
+        es.push_back(P(u, v));
+    }
+
+    void dfs(int x) {
+        if (visited[x]) continue;
+        for (int i : g[x]) dfs(i);
+        st.push(x);
+    }
+
+    void rdfs(int x, int k) {
+        if (num[x] != -1) continue;
+        num[x] = k;
+        for (int i : gr[x]) rdfs(i, k);
+    }
+    void build(vector<vector<int> > &t) {
+        for (int i = 0; i < g.size(); i++) dfs(i);
+        int p = 0;
+        while (!st.empty()) {
+            int v = st.top();
+            st.pop();
+            if (num[v] == -1) {
+                rdfs(v, p);
+                p++;
+            }
+        }
+        set<P> connected;
+        for (auto &e : es) {
+            int x = num[e.first], y = num[e.second];
+            if (x == y) continue;
+            if (connected.count({x, y})) continue;
+            t[x].push_back(y);
+            connected.insert({x, y});
+        }
+    }
+};
