69d3ffc053a5f043973f90ae4832629ac64d2dcf
NOSS <28915482+noss3885@users.noreply.github.com>
2018-08-05 12:29:21 +0900

    fix

--- src/graphs/shortest_path/bfs.cpp
+++ src/graphs/shortest_path/bfs.cpp
@@ -4,41 +4,44 @@
 グリッド状の迷路の最短経路を求める
 */
 
-#include<string>
-#include<queue>
+#include <string>
+#include <queue>
 
 using namespace std;
 
 #define INF 10000
 
 queue<int> px, py;
-int h, w, sx, sy, gx, gy, d[50][50], dx[4] = { 0,1,0,-1 }, dy[4] = { 1,0,-1,0 };
+int h, w, sx, sy, gx, gy, d[50][50], dx[4] = {0, 1, 0, -1},
+                                     dy[4] = {1, 0, -1, 0};
 string s[50];
 
-
 int bfs() {
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w; j++) {
-			d[i][j] = INF;
-		}
-	}
-	px.push(sx - 1); py.push(sy - 1);
-	d[sy][sx] = 0;
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            d[i][j] = INF;
+        }
+    }
+    px.push(sx - 1);
+    py.push(sy - 1);
+    d[sy][sx] = 0;
 
-	while (px.size()) {
-
-		int x = px.front(), y = py.front();
-		px.pop(); py.pop();
-		if (x == gx && y == gy) {
-			return d[y][x];
-		}
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i], ny = y + dy[i];
-			if (nx >= 0 && nx < w && ny >= 0 && ny < h && s[ny][nx] == '.' && d[ny][nx] == INF) {
-				d[ny][nx] = d[y][x] + 1;
-				px.push(nx); py.push(ny);
-			}
-		}
-	}
-	return INF;
+    while (px.size()) {
+        int x = px.front(), y = py.front();
+        px.pop();
+        py.pop();
+        if (x == gx && y == gy) {
+            return d[y][x];
+        }
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i], ny = y + dy[i];
+            if (nx >= 0 && nx < w && ny >= 0 && ny < h && s[ny][nx] == '.' &&
+                d[ny][nx] == INF) {
+                d[ny][nx] = d[y][x] + 1;
+                px.push(nx);
+                py.push(ny);
+            }
+        }
+    }
+    return INF;
 }
