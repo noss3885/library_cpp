cf316414edc60cdcf18ad70d1eaac042d0cb25c2
NOSS <28915482+noss3885@users.noreply.github.com>
2018-08-29 14:55:31 +0900

    fix

--- src/DP/LIS.cpp
+++ src/DP/LIS.cpp
@@ -1,18 +1,19 @@
 /*
 LIS 最長増加部分列
-O(N^2)
+O(NlogN)
 */
 #include <algorithm>
+#include <vector>
 using namespace std;
 
-const int MAX_N = 1000;
-int n, a[MAX_N], dp[MAX_N];
+const int IINF = INT_MAX;
+int n;
+vector<int> a, dp;
 
-void lis() {
-    for (int i = 0; i < n; i++) fill(dp[i], dp[i] + n, 0);
+int lis() {
+    dp.resize(n+1,IINF);
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < i; j++) {
-            if (a[i] > a[j]) dp[i + 1] = max(dp[i + 1], dp[j] + 1);
-        }
+        *lower_bound(dp.begin(),dp.end(),a[i]) = a[i];
     }
+    return lower_bound(dp.begin(),dp.end(),IINF) - dp.begin();
 }
