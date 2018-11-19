2ed6fd1781c682514b9e22c14a30d01e732d675d
NOSS <28915482+noss3885@users.noreply.github.com>
2018-11-19 11:33:48 +0900

    add fast prime factor

--- src/math/prime.cpp
+++ src/math/prime.cpp
@@ -4,8 +4,10 @@
 
 using namespace std;
 
+typedef long long ll;
+
 //素数判定
-bool is_prime(int n) {
+bool isprime(int n) {
     if (n % 2 == 0) return false;
     for (int i = 3; i * i <= n; i += 2) {
         if (n % i == 0) return false;
@@ -40,14 +42,43 @@ map<int, int> prime_factor(int n) {
 
 //エラトステネスの篩
 vector<bool> IsPrime;
+vector<int> primes;
 
-void sieve(size_t max) {
-    if (max + 1 > IsPrime.size()) {
-        IsPrime.resize(max + 1, true);
+void sieve(int max) {
+    IsPrime.resize(max + 1, true);
+    IsPrime[0] = IsPrime[1] = false;
+    for (int i = 2; i * i <= max; ++i){
+        if (IsPrime[i]){
+            for (int j = i * i; j <= max; j += i) IsPrime[j] = false;
+        }
     }
-    IsPrime[0] = false;
-    IsPrime[1] = false;
-    for (size_t i = 2; i * i <= max; ++i)
-        if (IsPrime[i])
-            for (size_t j = 2; i * j <= max; ++j) IsPrime[i * j] = false;
+    for(int i = 0; i <= max; ++i)if(IsPrime[i])primes.push_back(i);
+}
+
+//高速素因数分解
+vector<int> min_factor;
+
+void f_sieve(int max){
+    IsPrime.resize(max + 1, true);
+    min_factor.resize(max + 1, 0);
+    IsPrime[0] = IsPrime[1] = false;
+    min_factor[1] = 1;
+    for (int i = 2; i <= max; ++i)
+        if (IsPrime[i]){
+            primes.push_back(i);
+            for (ll j = ll(i) * i; j <= max; j += i){
+                IsPrime[j] = false;
+                min_factor[j] = i;
+            }
+        }
+}
+
+map<int, int> fast_prime_factor(int n){
+    map<int, int> res;
+    while(n > 1){
+        int factor = min_factor[n];
+        res[factor]++;
+        n /= factor;
+    }
+    return res;
 }
