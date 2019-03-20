c84d1af39ecdbdca531dcef3d299e091a7d79b04
NOSS <28915482+noss3885@users.noreply.github.com>
2019-03-21 00:08:58 +0900

    Modify BIT

--- src/data_structures/BIT.cpp
+++ src/data_structures/BIT.cpp
@@ -4,25 +4,31 @@ BIT
 区間の和を求める
 ある要素に加算する
 */
+#include<vector>
+using namespace std;
 
 struct BIT {
-    static const int MAX_N = 100000;
+    int N;
+    vector<int> bit;
 
-    int bit[MAX_N + 1]{0}, n;
+    void init(int _n){
+        N = _n;
+        bit.resize(_n, 0);
+    }
 
-    int sum(int i) {  // i番目までの要素の和を求める(1-index)
-        int s = 0;
+    int getSum(int i) {  // i番目までの要素の和を求める(1-index)
+        int sum = 0;
         while (i > 0) {
-            s += bit[i];
+            sum += bit[i];
             i -= i & -i;
         }
-        return s;
+        return sum;
     }
 
     void add(int i, int x) {  // i番目の要素にxを加算
-        while (i <= n) {
+        while (i <= N) {
             bit[i] += x;
             i += i & -i;
         }
     }
-}
+};
