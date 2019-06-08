6fc551628aab54442d15be2b3bb575558860fdcb
NOSS <28915482+noss3885@users.noreply.github.com>
2019-06-08 22:12:14 +0900

    Modyfy strongly connected components

--- src/graphs/strongly_connected_components.cpp
+++ src/graphs/strongly_connected_components.cpp
@@ -9,18 +9,30 @@ using namespace std;
 
 typedef pair<int, int> P;
 
-struct SCC {
-    vector<vector<int> > g, gr;
-    vector<int> visited, num;
-    vector<P> es;
+struct StronglyConnectedComponents {
+    int N;
+    vector<vector<int> > g, gr, g2i;
+    vector<bool> visited;
+    vector<int> i2g;
+    vector<P> edges;
     stack<int> st;
 
-    SCC(int n) : g(n), gr(n), visited(n, 0), num(n, -1) {}
-
+    StronglyConnectedComponents(){};
+    StronglyConnectedComponents(int n){init(n);}
+    void init(int n){
+        N = n;
+        g.clear();
+        g.resize(n);
+        gr.clear();
+        gr.resize(n);
+        edges.clear();
+        visited.resize(n);
+        i2g.resize(n);
+    }
     void add_edge(int u, int v) {
         g[u].push_back(v);
         gr[v].push_back(u);
-        es.push_back(P(u, v));
+        edges.push_back(P(u, v));
     }
 
     void dfs(int x) {
@@ -31,24 +43,32 @@ struct SCC {
     }
 
     void rdfs(int x, int k) {
-        if (num[x] != -1) return;
-        num[x] = k;
+        if (i2g[x] != -1) return;
+        i2g[x] = k;
         for (int i : gr[x]) rdfs(i, k);
     }
+
     void build(vector<vector<int> > &t) {
-        for (int i = 0; i < g.size(); i++) dfs(i);
+        fill(visited.begin(), visited.end(), false);
+        fill(i2g.begin(), i2g.end(), -1);
+        for (int i = 0; i < N; i++) dfs(i);
         int p = 0;
         while (!st.empty()) {
             int v = st.top();
             st.pop();
-            if (num[v] == -1) {
+            if (i2g[v] == -1) {
                 rdfs(v, p);
                 p++;
             }
         }
+        g2i.clear();
+        g2i.resize(p);
+        for(int i=0;i<N;i++){
+            g2i[i2g[i]].push_back(i);
+        }
         set<P> connected;
-        for (auto &e : es) {
-            int x = num[e.first], y = num[e.second];
+        for (auto &e : edges) {
+            int x = i2g[e.first], y = i2g[e.second];
             if (x == y) continue;
             if (connected.count({x, y})) continue;
             t[x].push_back(y);
