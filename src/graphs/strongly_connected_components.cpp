83963b7259e756790392da339e5fb3a03ca6ea32
NOSS <28915482+noss3885@users.noreply.github.com>
2019-09-23 09:13:39 +0900

    Modify SCC

--- src/graphs/strongly_connected_components.cpp
+++ src/graphs/strongly_connected_components.cpp
@@ -3,43 +3,40 @@
 計算量 O(V+E)
 */
 #include <vector>
+#include <algorithm>
 #include <stack>
-#include <set>
 using namespace std;
 
 typedef pair<int, int> P;
 
-struct StronglyConnectedComponents {
+struct SCC {
     int N;
-    vector<vector<int> > g, gr, g2i;
+    vector<vector<int> > g, gr, g2i, t;
     vector<bool> visited;
     vector<int> i2g;
-    vector<P> edges;
-    stack<int> st;
+    stack<int> order;
 
-    StronglyConnectedComponents(){};
-    StronglyConnectedComponents(int n){init(n);}
+    SCC(){};
+    SCC(int n){init(n);}
     void init(int n){
         N = n;
         g.clear();
         g.resize(n);
         gr.clear();
         gr.resize(n);
-        edges.clear();
         visited.resize(n);
         i2g.resize(n);
     }
     void add_edge(int u, int v) {
-        g[u].push_back(v);
-        gr[v].push_back(u);
-        edges.push_back(P(u, v));
+        g[u].emplace_back(v);
+        gr[v].emplace_back(u);
     }
 
     void dfs(int x) {
         if (visited[x]) return;
         visited[x] = true;
         for (int i : g[x]) dfs(i);
-        st.push(x);
+        order.push(x);
     }
 
     void rdfs(int x, int k) {
@@ -48,31 +45,31 @@ struct StronglyConnectedComponents {
         for (int i : gr[x]) rdfs(i, k);
     }
 
-    void build(vector<vector<int> > &t) {
+    void build() {
         fill(visited.begin(), visited.end(), false);
         fill(i2g.begin(), i2g.end(), -1);
         for (int i = 0; i < N; i++) dfs(i);
         int p = 0;
-        while (!st.empty()) {
-            int v = st.top();
-            st.pop();
-            if (i2g[v] == -1) {
-                rdfs(v, p);
-                p++;
-            }
+        while (!order.empty()) {
+            rdfs(order.top(), p++);
+            order.pop();
         }
         g2i.clear();
         g2i.resize(p);
         for(int i=0;i<N;i++){
             g2i[i2g[i]].push_back(i);
         }
-        set<P> connected;
-        for (auto &e : edges) {
-            int x = i2g[e.first], y = i2g[e.second];
-            if (x == y) continue;
-            if (connected.count({x, y})) continue;
-            t[x].push_back(y);
-            connected.insert({x, y});
+        t.resize(p);
+        for(int i=0;i<N;i++){
+            for (auto &to : g[i]) {
+                int x = i2g[i], y = i2g[to];
+                if (x == y) continue;
+                t[x].push_back(y);
+            }
+        }
+        for(int i=0;i<p;i++){
+            sort(t[i].begin(), t[i].end());
+            t[i].erase(unique(t[i].begin(), t[i].end()),t[i].end());
         }
     }
 };
