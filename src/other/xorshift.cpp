d66a0142af0c8cd6a05bd3c98a4e1b7ccf3d99bc
NOSS <28915482+noss3885@users.noreply.github.com>
2020-02-27 23:08:10 +0900

    Add Random class

--- src/other/xorshift.cpp
+++ src/other/xorshift.cpp
@@ -13,3 +13,31 @@ uint32_t xorshift(void) {
   x = y; y = z; z = w;
   return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)); 
 }
+
+struct Random {
+    using uint = unsigned int;
+    uint x = 123456789;
+    uint y = 362436069;
+    uint z = 521288629;
+    uint w = 88675123;
+    uint t;
+
+    uint rand(){
+        t = x ^ (x << 11);
+        x = y;
+        y = z;
+        z = w;
+        return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+    }
+    // [0,1)
+    double randFloat(){
+        return static_cast<double>(rand() % 0x10000) / static_cast<double>(0x10000);
+    }
+    // [aMin,aMax)
+    double randFloat(double aMin, double aMax){
+        return aMin + randFloat() * (aMax - aMin);
+    }
+    void spin(int n){
+        for(;n>0;--n) (void)rand();
+    }
+};
