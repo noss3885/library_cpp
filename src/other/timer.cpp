031ab866591d44fc0710323ac8aec3d282b295b0
NOSS <28915482+noss3885@users.noreply.github.com>
2020-04-10 09:39:28 +0900

    Add timer

---
+++ src/other/timer.cpp
@@ -0,0 +1,15 @@
+#include<chrono>
+
+class Timer{
+private:
+    std::chrono::system_clock::time_point start_time, end_time;
+public:
+    Timer():start_time(std::chrono::system_clock::now()){}
+    void start(){
+        start_time = std::chrono::system_clock::now();
+    }
+    double elapsed(){
+        end_time = std::chrono::system_clock::now();
+        return static_cast<double>(std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count());
+    }
+};
