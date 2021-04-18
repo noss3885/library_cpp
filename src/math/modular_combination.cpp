28f6c368ff1b5c5d6c3dc9b5b11d17c0b6254671
NOSS <28915482+noss3885@users.noreply.github.com>
2021-04-18 23:38:27 +0900

    Add Lucas's theorem

--- src/math/modular_combination.cpp
+++ src/math/modular_combination.cpp
@@ -25,6 +25,9 @@ ll mod_power(ll x, ll n) {  //繰り返し二乗法
 }
 
 void init(ll n) {
+    if(n >= MOD){
+        n = MOD-1;
+    }
     factrial.resize(n+1);
     inverse.resize(n+1);
     factrial[0] = 1;
@@ -37,5 +40,12 @@ void init(ll n) {
 
 ll nCk(ll n, ll k) {
     if(n < 0 || k < 0 || n < k) return 0;
-    return factrial[n] * inverse[k] % MOD * inverse[n - k] % MOD;
+    if(n < MOD) return factrial[n] * inverse[k] % MOD * inverse[n - k] % MOD;
+    // if n >= mod, use Lucas's theorem
+    ll res = 1;
+    while(n|k){
+        res = res * nCk(n%MOD, k%MOD) % MOD;
+        n /= MOD; k /= MOD;
+    }
+    return res;
 }
